8d1801c1fcbb0427ac25cac708707ef77942d025
nanrhe <hdhh@hotmail.com>
2022-08-26 19:51:22 -0500

    Creacion del proyecto y Clase MovimientoDinero

---
+++ src/main/java/org/UID/MovimientoDinero.java
@@ -0,0 +1,39 @@
+package org.UID;
+
+public class MovimientoDinero {
+
+    private Float montoMovimiento;
+    private String conceptoMovimiento;
+
+    //constructor
+
+    public MovimientoDinero(Float montoMovimiento, String conceptoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+
+    //Getter
+    public Float getMontoMovimiento() {
+        return montoMovimiento;
+    }
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+
+    //Setter
+    public void setMontoMovimiento(Float montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    //metodos
+
+    public void montos(){};
+    public void usuarioEncargado(){};
+
+
+}
